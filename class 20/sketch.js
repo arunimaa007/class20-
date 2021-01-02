e7366b194b293db681c772db39af5635d2f61fe8
arunimaa007 <76733596+arunimaa007@users.noreply.github.com>
2021-01-02 18:01:13 +0530

    Add files via upload

---
+++ class 20/sketch.js
@@ -0,0 +1,32 @@
+var fixedrect,movingrect
+
+
+
+function setup() {
+
+
+  createCanvas(800,400);
+  fixedrect=createSprite(400, 200, 50, 50);
+  movingrect=createSprite(400, 200, 30, 40);
+  fixedrect.shapeColor="turquoise";
+  movingrect.shapeColor="turquoise";
+}
+
+function draw() {
+  background(255,255,255);  
+  movingrect.x=World.mouseX;
+  movingrect.y=World.mouseY;
+  if(movingrect.x-fixedrect.x < fixedrect.width/2+movingrect.width/2 
+    && fixedrect.x-movingrect.x <fixedrect.width/2+movingrect.width/2
+    && movingrect.y-fixedrect.y< fixedrect.height/2+movingrect.height/2
+    && fixedrect.y-movingrect.y< fixedrect.height/2+movingrect.height/2 )
+ 
+    {
+       movingrect.shapeColor="pink";
+   } 
+   else{
+     movingrect.shapeColor="blue"; 
+    }
+
+  drawSprites();
+}
